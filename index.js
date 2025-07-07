f14dcea150d0611f5f5366457eacfa8fb20387e1
Alisa Lain <60989682+al1-ce@users.noreply.github.com>
2025-07-07 18:32:37 +0300

    fix routes 2

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const path            = require("path");
 
 const app = express();
 
-app.use( "/", express.static(path.join(__dirname, "public"), { extensions: ["html", "htm"] }));
 app.use( "/js", express.static(path.join(__dirname, "js"), { extensions: ["js"] }));
 app.use( "/css", express.static(path.join(__dirname, "css"), { extensions: ["css"] }));
+app.use( "/", express.static(path.join(__dirname, "public"), { extensions: ["html", "htm"] }));
 app.all("*", (_, res) => { res.status(404).sendFile(path.join(__dirname, "public/404.html")); });
 
 const port = process.env.PORT || 3000;
